69f9b9f0b063d312de49d2b90589e88e7cd41c71
zsikorska <sikorska.zuzia@gmail.com>
2022-09-29 09:33:02 +0100

    copy necessary files from lab1_2 to lab1_3

---
+++ lab1/lab1_3/my_weather_radar/src/test/java/com/myweatherradar/WeatherStarterTest.java
@@ -0,0 +1,19 @@
+package com.myweatherradar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class WeatherStarterTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
