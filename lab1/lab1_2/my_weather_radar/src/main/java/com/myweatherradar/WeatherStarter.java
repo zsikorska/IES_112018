a2fffcc428f59d0b57ee9425da4fcfe52f1439f2
zsikorska <sikorska.zuzia@gmail.com>
2022-09-29 04:52:53 +0100

    update pom.xml and WeatherStarter in lab1_2

--- lab1/lab1_2/my_weather_radar/src/main/java/com/myweatherradar/WeatherStarter.java
+++ lab1/lab1_2/my_weather_radar/src/main/java/com/myweatherradar/WeatherStarter.java
@@ -16,6 +16,13 @@ public class WeatherStarter {
 
     public static void  main(String[] args ) {
 
+        int city_id;
+
+        if (args.length == 0)
+            city_id = CITY_ID_AVEIRO;
+        else
+            city_id = Integer.parseInt(args[0]);
+
         // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.ipma.pt/open-data/")
@@ -25,7 +32,7 @@ public class WeatherStarter {
         // create a typed interface to use the remote API (a client)
         IpmaService service = retrofit.create(IpmaService.class);
         // prepare the call to remote endpoint
-        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(city_id);
 
         try {
             Response<IpmaCityForecast> apiResponse = callSync.execute();
@@ -34,9 +41,11 @@ public class WeatherStarter {
             if (forecast != null) {
                 var firstDay = forecast.getData().listIterator().next();
 
-                System.out.printf( "max temp for %s is %4.1f %n",
-                        firstDay.getForecastDate(),
-                        Double.parseDouble(firstDay.getTMax()));
+                System.out.printf("Forecast for the %s:%n", firstDay.getForecastDate());
+                System.out.printf( "- max temp is %4.1f %n", Double.parseDouble(firstDay.getTMax()));
+                System.out.printf( "- min temp is %4.1f %n", Double.parseDouble(firstDay.getTMin()));
+                System.out.printf( "- predicted wind direction is %s %n", firstDay.getPredWindDir());
+
             } else {
                 System.out.println( "No results for this request!");
             }
